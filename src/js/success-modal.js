adc651f7ac84e5e4d36aa133ae782b1a79c7f301
yanamikha <yanamikha@gmail.com>
2026-09-25 15:07:18 +0200

    Додано адаптивну верстку. Згідно ТЗ виконано: Закривати по backdrop.
    Закривати по Escape.
    
    Блокувати scroll сторінки при відкритому modal.
    
    Повертати scroll після закриття.

--- src/js/success-modal.js
+++ src/js/success-modal.js
@@ -3,12 +3,34 @@
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
+    body: document.body,
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  if (!refs.openModalBtn || !refs.closeModalBtn || !refs.modal || !refs.body) {
+    return;
+  }
+
+  refs.openModalBtn.addEventListener('click', openModal);
+  refs.closeModalBtn.addEventListener('click', closeModal);
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
+  }
+
+  function onEscapePress(event) {
+    if (event.code === 'Escape') {
+      closeModal();
+    }
+  }
+
+  function openModal() {
+    toggleModal();
+    window.addEventListener('keydown', onEscapePress);
+  }
+
+  function closeModal() {
+    toggleModal();
+    window.removeEventListener('keydown', onEscapePress);
   }
 })();
